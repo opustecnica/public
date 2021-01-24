8350f2ce1421dd630bd60851ea3ec77d6dbb4e7d
opustecnica <jcrisafulli@gmail.com>
2021-01-23 20:19:43 -0500

    Update mac_spoofing.sh

--- UDM/mac_spoofing.sh
+++ UDM/mac_spoofing.sh
@@ -2,6 +2,4 @@
 
 MAC='20:c0:47:42:0a:bf'
 IF=$(ip route show table 201 | awk '{print $5}')
-ip link set dev $IF down
-ip link set dev $IF address $MAC
-ip link set dev $IF up
+ip link set dev $IF down && ip link set dev $IF address $MAC && ip link set dev $IF up
